348b6d29082f71edaaef869b229f60fd7dc0991c
Noé Merle <noemerle@MacBook-Air-de-Noe.local>
2026-03-04 14:44:34 +0100

    [FIX] build & test

--- src/components/LaunchpadForm.tsx
+++ src/components/LaunchpadForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Play, Loader2, AlertCircle } from "lucide-react";
 
 export interface CreateScanRequest {
