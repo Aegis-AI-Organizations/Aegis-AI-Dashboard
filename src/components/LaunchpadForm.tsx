bc5a9d0ee1bb1e4f59fa5329109ddc67f6695db5
Noé Merle <noemerle@MacBook-Air-de-Noe.local>
2026-03-04 14:41:28 +0100

    [FIX] build & test

--- src/components/LaunchpadForm.tsx
+++ src/components/LaunchpadForm.tsx
@@ -16,7 +16,7 @@ export const LaunchpadForm: React.FC = () => {
   const [targetImage, setTargetImage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-//   const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
