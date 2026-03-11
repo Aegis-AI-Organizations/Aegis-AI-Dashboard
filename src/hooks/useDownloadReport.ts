f8ae18c02be3c116fbbddf382d600b0b8a0a5f50
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2026-03-11 23:56:48 +0100

    [FIX] log download pdf

--- src/hooks/useDownloadReport.ts
+++ src/hooks/useDownloadReport.ts
@@ -5,40 +5,22 @@ export const useDownloadReport = () => {
   const [isDownloading, setIsDownloading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  const downloadReport = async (scanId: string) => {
+  const downloadReport = (scanId: string) => {
     setIsDownloading(true);
     setError(null);
 
     try {
-      const response = await fetch(
-        `${config.apiGatewayUrl}/scans/${scanId}/report`,
-        {
-          method: "GET",
-          headers: {
-            Accept: "application/pdf",
-          },
-        },
-      );
-
-      if (!response.ok) {
-        throw new Error(`Erreur lors du téléchargement: ${response.status}`);
-      }
-
-      const blob = await response.blob();
-
-      const url = window.URL.createObjectURL(blob);
-
+      const url = `${config.apiGatewayUrl}/scans/${scanId}/report`;
       const link = document.createElement("a");
       link.href = url;
       link.setAttribute("download", `Aegis-Report-${scanId}.pdf`);
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-      window.URL.revokeObjectURL(url);
     } catch (err: any) {
       console.error("Erreur lors du téléchargement du rapport :", err);
       setError(
-        "Impossible de télécharger le rapport. Veuillez réessayer plus tard.",
+        "Impossible de lancer le téléchargement. Veuillez réessayer plus tard.",
       );
     } finally {
       setIsDownloading(false);
